a35ec1c3cd62fc21379d23ef58b5f6148293927b
Naveen-Winter <naveenkrishna38@gmail.com>
2022-06-23 20:08:58 -0700

    Update valid_prathesis.py
    new changes : but still fails

--- valid_prathesis.py
+++ valid_prathesis.py
@@ -1,29 +1,24 @@
-
-#Solution is accepted 
-
-
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        opening = set(['(','{','['])
-        closing = set([')',']','}'])
+        
         pranth_dict = {'(':')', '{':'}','[':']' }
         track_closing = []
-        print('here')
         if len(s):
-            print('here')
             for pranth in s:
-                print(pranth)
-                print(track_closing)
+                
                 if pranth in pranth_dict:
                     track_closing.append(pranth_dict[pranth])
-                else:
-                    if track_closing[0] == pranth:
+                if pranth not in pranth_dict:
+                    print(track_closing[0])
+                    if pranth == track_closing[0]:
                         track_closing.pop()
+                    else:
+                        return False
+                print(track_closing)
             if len(track_closing) == 0:
                 return True
         return False
-            
