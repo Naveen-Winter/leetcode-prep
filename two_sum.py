5f531ef5506ca1f3bf5bb435e07048f13860466f
Naveen-Winter <naveenkrishna38@gmail.com>
2022-06-24 19:22:39 -0700

    Update two_sum.py
    Added comments

--- two_sum.py
+++ two_sum.py
@@ -6,6 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
         
+
         if len(nums) > 1:
             target_pair = dict()
             
@@ -16,5 +17,8 @@ class Solution(object):
                 if nums[i] in target_pair:
                     return[i, target_pair[nums[i]]]
                 else:
+                    # diff = target - nums[i]
+                    # we are trying to find if a number diff exists as nums[j]
+                    # so we can return [i,j]. For now lets just store i while we look for j
                     target_pair[diff] = i
                 
